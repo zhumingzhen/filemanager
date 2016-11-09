96e6733b7bc53724a539ec007876063197c926fc
mingzhen <mingzhen@yihe-inc.com>
2016-11-10 00:06:45 +0800

    add index

--- uploading.php
+++ uploading.php
@@ -1,15 +1,2 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<title>文件上传</title>
-</head>
-<body>
-	<div class="content">
-		<form action="" method="post" enctype=”multipart/form-data”>
-			<input type="file">
-			<input type="submit">
-		</form>
-	</div>
-</body>
-</html>
+<?php
+echo $_GET();
